8087d578acce237fc02b19ae44a1653fa02ee5f1
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2021-11-21 14:36:01 +0200

    Change colors

--- app/src/main/java/PolyTetris/pieces/Type.java
+++ app/src/main/java/PolyTetris/pieces/Type.java
@@ -11,27 +11,29 @@ import java.awt.Color;
  * @author Ilari
  */
 public enum Type {
-    MONOMINO(Color.GREEN, new int[][]{}),
-    DOMINO(Color.GREEN, new int[][]{{0, 1}}),
-    TROMINO_L(Color.YELLOW, new int[][]{{1, 0}, {0, 1}}),
-    TROMINO_I(Color.YELLOW, new int[][]{{0, 1}, {0, -1}}),
-    TETRA_I(new Color(250, 180, 44), new int[][]{{0, 1}, {0, -1}, {0, -2}}),
-    TETRA_L(new Color(250, 180, 44), new int[][]{{0, 1}, {0, -1}, {1, -1}}),
-    TETRA_T(new Color(250, 180, 44), new int[][]{{-1, 0}, {1, 0}, {0, 1}}),
-    TETRA_O(new Color(250, 180, 44), new int[][]{{-1, 1}, {0, 1}, {-1, 0}}),
-    TETRA_S(new Color(250, 180, 44), new int[][]{{-1, 0}, {0, 1}, {1, 1}}),
-    PENTA_F(Color.RED, new int[][]{{-1,0},{0,1},{0,-1},{1,-1}}),
-    PENTA_I(Color.RED, new int[][]{{-2,0},{-1,0},{1,0},{2,0}}),
-    PENTA_L(Color.RED, new int[][]{{-1,-1},{-1,0},{1,0},{2,0}}),
-    PENTA_N(Color.RED, new int[][]{{-1,1},{0,1},{1,0},{2,0}}),
-    PENTA_P(Color.RED, new int[][]{{-1,0},{-1,1},{0,1},{0,-1}}),
-    PENTA_T(Color.RED, new int[][]{{-1,-1},{0,-1},{1,-1},{0,1}}),
-    PENTA_U(Color.RED, new int[][]{{-1,0},{-1,-1},{1,0},{1,-1}}),
-    PENTA_V(Color.RED, new int[][]{{2,0},{1,0},{0,-1},{0,-2}}),
-    PENTA_W(Color.RED, new int[][]{{-1,0},{-1,-1},{0,1},{1,1}}),
-    PENTA_X(Color.RED, new int[][]{{-1,0},{1,0},{0,-1},{0,1}}),
-    PENTA_Y(Color.RED, new int[][]{{-1,0},{0,-1},{1,0},{2,0}}),
-    PENTA_Z(Color.RED, new int[][]{{-1,-1},{0,-1},{0,1},{1,1}});
+    MONOMINO(Type.color(0f), new int[][]{}),
+    DOMINO(Type.color(0.5f), new int[][]{{0, 1}}),
+    TROMINO_L(Type.color(0.25f), new int[][]{{1, 0}, {0, 1}}),
+    TROMINO_I(Type.color(0.75f), new int[][]{{0, 1}, {0, -1}}),
+
+    TETRA_I(Type.color(0.05f), new int[][]{{0, 1}, {0, -1}, {0, -2}}),
+    TETRA_L(Type.color(0.15f), new int[][]{{0, 1}, {0, -1}, {1, -1}}),
+    TETRA_T(Type.color(0.35f), new int[][]{{-1, 0}, {1, 0}, {0, 1}}),
+    TETRA_O(Type.color(0.55f), new int[][]{{-1, 1}, {0, 1}, {-1, 0}}),
+    TETRA_S(Type.color(0.85f), new int[][]{{-1, 0}, {0, 1}, {1, 1}}),
+
+    PENTA_F(Type.color(0.1f), new int[][]{{-1,0},{0,1},{0,-1},{1,-1}}),
+    PENTA_I(Type.color(0.2f), new int[][]{{-2,0},{-1,0},{1,0},{2,0}}),
+    PENTA_L(Type.color(0.3f), new int[][]{{-1,-1},{-1,0},{1,0},{2,0}}),
+    PENTA_N(Type.color(0.4f), new int[][]{{-1,1},{0,1},{1,0},{2,0}}),
+    PENTA_P(Type.color(0.45f), new int[][]{{-1,0},{-1,1},{0,1},{0,-1}}),
+    PENTA_T(Type.color(0.5f), new int[][]{{-1,-1},{0,-1},{1,-1},{0,1}}),
+    PENTA_U(Type.color(0.6f), new int[][]{{-1,0},{-1,-1},{1,0},{1,-1}}),
+    PENTA_V(Type.color(0.65f), new int[][]{{2,0},{1,0},{0,-1},{0,-2}}),
+    PENTA_W(Type.color(0.7f), new int[][]{{-1,0},{-1,-1},{0,1},{1,1}}),
+    PENTA_X(Type.color(0.8f), new int[][]{{-1,0},{1,0},{0,-1},{0,1}}),
+    PENTA_Y(Type.color(0.9f), new int[][]{{-1,0},{0,-1},{1,0},{2,0}}),
+    PENTA_Z(Type.color(0.95f), new int[][]{{-1,-1},{0,-1},{0,1},{1,1}});
 
     private int[][] positions;
     private Color color;
@@ -39,6 +41,10 @@ public enum Type {
     private Type(Color c, int [][] positions){
         this.color = c;
         this.positions = positions;
+    }
+
+    private static Color color(float hue) {
+        return new Color(Color.HSBtoRGB(hue, 1f, 0.9f));
     }
     
     public int[][] getPositions(boolean reverse){ 
